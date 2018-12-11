2144254b446109e0e3d0053e9030e4c42b77f572
Tiffani Dominguez <tiffanid@tiffanis-mbp.attlocal.net>
2018-12-11 10:59:33 -0600

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,56 @@
-// Create an array of Words
-var words = ['boston terrier', 'bulldog', 'poodle', 'boxer', 'pug', 'husky', 'chihuahua']
-// Choose word randomly
-var randNum  = Math.floor(Math.random() * words.length);
-var chosenWord = word[randNum];
-console.log(chosenWord);
-// Create underscores based on length of word
-var generateUnderscore = () => {
+$(document).ready(function() {
+ // Create an array of Words
+ var words = ['scorpion', 'raiden', 'kitana', 'tanya', 'sub-zero', 'reptile', 'mileena']
+ // Choose word randomly
+ var randNum  = Math.floor(Math.random() * words.length);
+ var chosenWord = words[randNum];
+ var rightWord = [];
+ var wrongWord = [];
+ var underScore = [];
+ var docUnderScore = document.getElementsByClassName('underScore');
+
+ // Gets Link for Theme Song
+ var audioElement = document.createElement("audio");
+ audioElement.setAttribute("src", "assets/audio/Mortal Kombat.mp3");
+
+ // Theme Button
+ $(".play-button").on("click", function() {
+   audioElement.play();
+ });
+ $(".pause-button").on("click", function() {
+   audioElement.pause();
+ });  
+
+
+ // Scoreboard
+ var wins = 0;
+ var losses = 0;
+ var guessesRemaining = 9;
+
+
+ // Create underscores based on length of word
+ var generateunderScore = () => {
     for(var i = 0; i < chosenWord.length; i++){
-        Underscore.push('_');
+        underScore.push('_');
     }
-    return Underscore;
-}
+    return underScore;
+ }
 
-//Get users guess
-document.addEventListener('keypress', (event) => {
+ //Get users guess
+ document.addEventListener('keypress', (event) => {
     var keyword = String.fromCharCode(event.keycode);
-    if(keyword.indexOf(chosenWord) > -1 {
-        console.log(true);
+ // if users guess is right  
+    if(chosenWord.indexOf(keyword) > -1) {
+    // add to right words array
+        rightWord.push(keyword);
+    // replace underscore with right word  
+        underScore[chosenWord.indexOf(keyword)] = keyword;
+        docUnderScore[0].innerHTML = underScore.join(' ');
+    // check users guess
+        if(underScore.join('') == chosenWord) {
+            alert('You Win!');
+        }   
     }
-
-    
-});
-//Check if Guess is right
-//If right push to right array
-//If wring push to wrong array
+    else wrongWord.push(keyword); {
+        alert('Try again!');
+    });
